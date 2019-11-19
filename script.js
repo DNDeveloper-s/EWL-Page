8b39fff86da8835c0703e6ae98576c1bb895e7b7
yvglimited <57485419+yvglimited@users.noreply.github.com>
2019-11-19 12:12:12 +0530

    yash big  changes

---
+++ script.js
@@ -0,0 +1,80 @@
+let moving__line = document.querySelector('.moving__line');
+let wrapper = document.querySelector('.wrapper');
+let listItems = document.querySelectorAll('.list__items');
+let navigation = document.querySelector('.navigation');
+let menu__icon = document.querySelector('.menu__icon');
+let nav__itemsContainer = document.querySelector('.nav__items');
+
+function removeClassFromSiblings(classes, element) {
+    element.forEach(cur => {
+        cur.classList.remove(classes);
+    })
+}
+
+function updateMovingLine() {
+    let activeListItem = document.querySelector('.nav__items ul li.active');
+    let activeCoords = activeListItem.getClientRects();
+    let wrapperCoords = wrapper.getClientRects();
+
+    moving__line.style.left = `${activeCoords[0].left - wrapperCoords[0].left}px`;
+    moving__line.style.width = `${activeCoords[0].width}px`;
+}
+
+updateMovingLine();
+
+listItems.forEach(cur => {
+    cur.addEventListener('click', function (e) {
+        smoothScroll(cur);
+        removeClassFromSiblings('active', listItems);
+        e.preventDefault();
+        this.classList.add('active');
+        updateMovingLine();
+    });
+});
+
+window.addEventListener('resize', updateMovingLine);
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 148) {
+        navigation.classList.add('collapse');
+    } else {
+        navigation.classList.remove('collapse');
+    }
+});
+
+window.addEventListener('wheel', loopThroughListItems);
+
+function loopThroughListItems() {
+    listItems.forEach(cur => {
+        updateNavOnScroll(`${cur.dataset.id}`);
+    })
+}
+
+menu__icon.addEventListener('click', () => {
+    nav__itemsContainer.classList.toggle('mobDropDown');
+});
+
+document.addEventListener('click', e => {
+    if(!e.path.includes(navigation)) {
+        nav__itemsContainer.classList.remove('mobDropDown');
+    }
+});
+
+function updateNavOnScroll(curE) {
+    let curEl = document.querySelector(`.${curE}`);
+    let curListItem = document.querySelector(`.list__items[data-id=${curE}]`);
+    if((curEl.offsetTop - window.pageYOffset) <= 640 && (curEl.offsetTop + curEl.clientHeight - window.pageYOffset) > 0) {
+        removeClassFromSiblings('active', listItems);
+        curListItem.classList.add('active');
+        updateMovingLine();
+    }
+}
+
+function smoothScroll(curEl) {
+    let targetDiv = document.querySelector(`.${curEl.dataset.id}`);
+    window.scrollTo({
+        top: targetDiv.offsetTop - 210,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
